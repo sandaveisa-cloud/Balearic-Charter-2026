36e94bd2f6f29f5921cf2b250871d21e3619f00e
sandaveisa-cloud <sandaveisa-cloud@users.noreply.github.com>
2026-01-23 10:38:18 +0100

    Fix AdminSidebar: Remove duplicate 'use client' and merge duplicate imports

--- components/AdminSidebar.tsx
+++ components/AdminSidebar.tsx
@@ -2,10 +2,6 @@
 
 import Link from 'next/link'
 import { usePathname } from 'next/navigation'
-'use client'
-
-import Link from 'next/link'
-import { usePathname } from 'next/navigation'
 import { useState, useEffect } from 'react'
 import { 
   LayoutDashboard, 
